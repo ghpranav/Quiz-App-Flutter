c51abb79ccfe628ef87afa3cd2425dc7c380825e
YPrahasith <lohit2552@gmail.com>
2021-07-08 11:50:46 +0530

    Edited score_screen.dart

--- lib/screens/score/score_screen.dart
+++ lib/screens/score/score_screen.dart
@@ -5,6 +5,7 @@ import 'package:autism_world/controllers/question_controller.dart';
 import 'package:flutter_svg/svg.dart';
 
 class ScoreScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     QuestionController _qnController = Get.put(QuestionController());
@@ -17,10 +18,10 @@ class ScoreScreen extends StatelessWidget {
             children: [
               Spacer(flex: 3),
               Text(
-                "Score",
+                "These results indicate that ...",
                 style: Theme.of(context)
                     .textTheme
-                    .headline3!
+                    .headline5!
                     .copyWith(color: kSecondaryColor),
               ),
               Spacer(),
@@ -31,7 +32,21 @@ class ScoreScreen extends StatelessWidget {
                     .headline4!
                     .copyWith(color: kSecondaryColor),
               ),
-              Spacer(flex: 3),
+              Spacer(flex:1),
+               LinearProgressIndicator(
+                 value:0.5,
+               ),
+              Spacer(flex:1),
+              Text(
+                "Your score suggests you experience many or all of the most common traits experienced by those with an autism spectrum disorder.",
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: kSecondaryColor),
+
+              ),
+              Spacer(flex:5),
             ],
           )
         ],
